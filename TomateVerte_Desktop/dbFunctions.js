cc4993ef989be068d80465e3c25ba044cd873081
PiKSo05 <vilain.vincent@gmail.com>
2018-07-14 23:23:40 +0200

    correction de la synchroniation des fonctions

--- TomateVerte_Desktop/dbFunctions.js
+++ TomateVerte_Desktop/dbFunctions.js
@@ -1,41 +1,52 @@
 
 var Datastore = require('nedb')
-, db = new Datastore({ filename: 'data_storage/TomateVerte.nedb', autoload: true });
+    , db = new Datastore({ filename: 'data_storage/TomateVerte.nedb', autoload: true });
 
 module.exports = {
     //update parameter value
-    setParam : function (paramName, paramValue){
+    setParam: function (paramName, paramValue) {
         console.log('update parameter value : ' + paramName + ' = ' + paramValue);
-        db.update({name: paramName}, { name: paramName, value: paramValue}, { upsert: true }, function () {
+        db.update({ name: paramName }, { name: paramName, value: paramValue }, { upsert: true }, function () {
             console.log('update parameter value' + paramName + ' = ' + paramValue + ' - SUCCESS');
         });
     },
     //get parameter value
-    getParam : function (paramName){
+    getParam: function (paramName, callback) {
         console.log('get parameter value : ' + paramName);
 
-        // var docs = db.find({name: 'ipRaspberry'});
-
+        // var docs = db.find({ name: paramName });
         // if (docs.length !== 0) {
         //     var paramValue = docs[0].value;
         //     console.log('get parameter value : ' + paramName + ' = ' + paramValue);
         //     return paramValue;
         // }
-        // else{
+        // else {
         //     console.log('paramètre inexistant : ' + paramName);
         //     return null;
         // }
-
-        db.find({name: 'ipRaspberry'}, function (err, docs) {
+        db.find({ name: paramName },  (err, docs) => {
             if (docs.length !== 0) {
                 var paramValue = docs[0].value;
                 console.log('get parameter value : ' + paramName + ' = ' + paramValue);
+                callback(paramValue);
                 return paramValue;
             }
-            else{
+            else {
                 console.log('paramètre inexistant : ' + paramName);
                 return null;
             }
         });
+
+        // db.find({name: paramName}, function (err, docs) {
+        //     if (docs.length !== 0) {
+        //         var paramValue = docs[0].value;
+        //         console.log('get parameter value : ' + paramName + ' = ' + paramValue);
+        //         return paramValue;
+        //     }
+        //     else{
+        //         console.log('paramètre inexistant : ' + paramName);
+        //         return null;
+        //     }
+        // });
     }
 }
